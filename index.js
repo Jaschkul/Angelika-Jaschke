8d7020a524b5d73d94ed2d9a4afa0a07d8840e69
Jaschkul <u.jaschke1@gmail.com>
2026-03-20 14:49:30 +0100

    diashow

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
+// diashow 
+
+const bilder = document.querySelectorAll(".bilder img");
+let bildIndex = 0; 
+let intervalId = null;
+
+// initialisierung
+initiatlsierung(); 
+function initiatlsierung(){
+    if(bilder.length > 0){
+    bilder[bildIndex].classList.add("bildAnzeige");
+    intervalId = setInterval(bildDanach, 5000);}
+}  
+
+// aktuelle Seite  
+function zeigeBild(index){
+    
+    if(index >=  bilder.length){
+        bildIndex = 0;
+    }
+    else if(index <0 ){
+        bildIndex = bilder.length -1;
+    }
+    
+    //bildIndex = 0; 
+
+    //bildIndex = bilder.length -1;
+
+    bilder.forEach(bild => {
+        bild.classList.remove("bildAnzeige");
+    });
+    bilder[bildIndex].classList.add("bildAnzeige");
+}
+
+// Steuerung
+function bildDavor(){
+    clearInterval(intervalId);
+    bildIndex --;
+    zeigeBild(bildIndex);
+}
+function bildDanach(){
+    clearInterval(intervalId);
+    bildIndex ++;
+    zeigeBild(bildIndex);
+}
+
+// mehr infos 
 let mehr = document.getElementById("mehr");
-let infos = document.getElementById("infos"); 
+let infos = document.getElementById("infos");
 
-let aktiv = document.getElementById("aktiv");
-let zurück = document.getElementById("zurück");
-let weiter = document.getElementById("weiter");
-let gallerien = ["gallerie1.html", "gallerie2.html", "gallerie3.html"];
-let steuerungselement = document.getElementById("steuerungselement");
-
-
-// Text anzeigen und ausblenden
 infos.style.display = "none";
-mehr.addEventListener("click", event =>{
-    if(infos.style.display === "none"){ 
-        infos.style.display = "block"; 
-        mehr.textContent = "zurück";
-    } 
-    else{ 
-        infos.style.display = "none"; 
+
+mehr.addEventListener("click", event => {
+    if(mehr.textContent === "mehr"){
+        infos.style.display = "block";
+        mehr.textContent = "weniger";
+    }
+    else{
+        infos.style.display = "none";
         mehr.textContent = "mehr";
     }
 });
 
-//Steuerungselementent
-
-let hi = document.getElementById("hi");
-
-hi.textContent = "Hallo";
